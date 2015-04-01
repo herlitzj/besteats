55ae2831bd1a5a963ae9efeeff9cefe076f3f6be
Jeff <herlitzj@gmail.com>
2015-04-01 08:29:11 -0400

    Update gayotCrawler.rb

--- gayotCrawler.rb
+++ gayotCrawler.rb
@@ -16,27 +16,27 @@ end
 #scrapes name, neighborhood, street address, etc. out of review
 def scraper(url2)
   dataUrl = Nokogiri::HTML(open(url2))
-  linksUrl = dataUrl.css('section.content-unit')
+  linksUrl = dataUrl.css('div#leftContainer')
   
     linksUrl.each do |review|
       arr = []
       # name
-      arr << review.css('div#nameAddress h1.item').text.strip
+      arr << review.css('span.fn').text.strip
       # neighborhood
       #arr << review.css('span.neighborhood').text.strip
       # street-address
-      #arr << review.css('span.street-address').text.strip
+      arr << review.css('div.h5').first.text.strip.split("Send").first
       # street-address
       #arr << review.css('span.locality').text.strip
       # price rating
-      #arr << review.css('span.price').text.strip
+      arr << review.css('span.pricerange').text.strip
       # rating
-      arr << review.css('div#ratingPrice div.h6 a.v2').text.strip
-      #strong
-      #arr << review.css('p').css('strong').text
+      arr << review.css('span.value').text.strip
+      #blurb
+      arr << review.css('span#blurb').text.strip
 
       return arr
-    end    
+    end     
 end
 
 linkReturn("http://www.gayot.com/new-restaurants/new-york-area_1ny.html")
